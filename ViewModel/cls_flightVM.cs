2723c6e64ce6641edcba52f50409d6554b97957b
EvaMariaK <93390175+EvaMariaK@users.noreply.github.com>
2023-10-01 22:04:35 +0300

    The final work!

---
+++ ViewModel/cls_flightVM.cs
@@ -0,0 +1,13 @@
+﻿using Project___Intro_To_Computer_Networking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project___Intro_To_Computer_Networking.ViewModel
+{
+    public abstract class cls_flightVM
+    {
+        public abstract bool IsOneWayFlight();
+    }
+}
